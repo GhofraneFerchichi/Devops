3b2f6cf7d99d69f109649e89ae186ee4a28b1dbe
oussamasaihi <seihioussama2@gmail.com>
2023-10-15 23:39:06 +0100

    oussama

--- src/test/java/tn/esprit/rh/achat/ProduitServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/ProduitServiceImplTest.java
@@ -1,26 +1,40 @@
 package tn.esprit.rh.achat;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import tn.esprit.rh.achat.entities.Produit;
 import tn.esprit.rh.achat.repositories.ProduitRepository;
-import tn.esprit.rh.achat.repositories.StockRepository;
 import tn.esprit.rh.achat.services.ProduitServiceImpl;
-import tn.esprit.rh.achat.services.StockServiceImpl;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
 public class ProduitServiceImplTest {
-    @Autowired
-    ProduitServiceImpl produitService ;
-    @Autowired
-    StockServiceImpl stockService;
-    @MockBean
-    StockRepository SR ;
-    @MockBean
-    ProduitRepository PR;
+    @InjectMocks
+    private ProduitServiceImpl produitService;
+
+    @Mock
+    private ProduitRepository produitRepository;
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void testretrieveAllProduits(){
+        Mockito.when(produitRepository.findAll()).thenReturn(Arrays.asList(new Produit(), new Produit()));
+        List<Produit> produits = produitService.retrieveAllProduits();
+        assertEquals(2, produits.size());
+    }
 
 
 }
