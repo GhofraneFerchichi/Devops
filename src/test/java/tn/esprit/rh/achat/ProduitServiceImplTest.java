50ec08597d5d512e16011ce5f05c6e08b429a925
oussamasaihi <seihioussama2@gmail.com>
2023-10-18 15:54:51 +0100

    oussama

--- src/test/java/tn/esprit/rh/achat/ProduitServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/ProduitServiceImplTest.java
@@ -5,17 +5,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.rh.achat.entities.Produit;
+import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.repositories.ProduitRepository;
+import tn.esprit.rh.achat.repositories.StockRepository;
 import tn.esprit.rh.achat.services.ProduitServiceImpl;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
@@ -25,15 +31,74 @@ public class ProduitServiceImplTest {
 
     @Mock
     private ProduitRepository produitRepository;
+    @Mock
+    private StockRepository stockRepository ;
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
     }
     @Test
     public void testretrieveAllProduits(){
-        Mockito.when(produitRepository.findAll()).thenReturn(Arrays.asList(new Produit(), new Produit()));
+        when(produitRepository.findAll()).thenReturn(Arrays.asList(new Produit(), new Produit()));
         List<Produit> produits = produitService.retrieveAllProduits();
         assertEquals(2, produits.size());
+    }
+    @Test
+    public void testAddProduit() {
+        Produit produit = new Produit();
+
+        when(produitRepository.save(any(Produit.class))).thenReturn(produit);
+
+        Produit result = produitService.addProduit(produit);
+
+        assertEquals(produit, result);
+    }
+    @Test
+    public void testDeleteProduit() {
+        Long produitId = 1L;
+
+        produitService.deleteProduit(produitId);
+
+        verify(produitRepository).deleteById(produitId);
+    }
+
+    @Test
+    public void testUpdateProduit() {
+        Produit produit = new Produit();
+
+        when(produitRepository.save(any(Produit.class))).thenReturn(produit);
+
+        Produit result = produitService.updateProduit(produit);
+
+        assertEquals(produit, result);
+    }
+
+    @Test
+    public void testRetrieveProduit() {
+        Long produitId = 1L;
+        Produit produit = new Produit();
+
+        when(produitRepository.findById(produitId)).thenReturn(Optional.of(produit));
+
+        Produit result = produitService.retrieveProduit(produitId);
+
+        assertEquals(produit, result);
+    }
+
+    @Test
+    public void testAssignProduitToStock() {
+        Long idProduit = 1L;
+        Long idStock = 2L;
+        Produit produit = new Produit();
+        Stock stock = new Stock();
+
+        when(produitRepository.findById(idProduit)).thenReturn(Optional.of(produit));
+        when(stockRepository.findById(idStock)).thenReturn(Optional.of(stock));
+
+        produitService.assignProduitToStock(idProduit, idStock);
+
+        assertEquals(stock, produit.getStock());
+        verify(produitRepository).save(produit);
     }
 
 
