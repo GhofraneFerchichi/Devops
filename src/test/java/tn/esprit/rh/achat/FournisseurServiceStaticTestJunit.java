3281210de5fe54d5c53dfbac5394fcaa964be2fd
GhofraneFerchchi <ghofranefer01@gmail.com>
2023-11-04 21:10:38 +0100

    fixing files

---
+++ src/test/java/tn/esprit/rh/achat/FournisseurServiceStaticTestJunit.java
@@ -0,0 +1,106 @@
+package tn.esprit.rh.achat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import tn.esprit.rh.achat.entities.CategorieFournisseur;
+import tn.esprit.rh.achat.entities.Fournisseur;
+import tn.esprit.rh.achat.repositories.FournisseurRepository;
+import tn.esprit.rh.achat.services.FournisseurServiceImpl;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+@Transactional
+public class FournisseurServiceStaticTestJunit {
+    @Autowired
+    private FournisseurServiceImpl fournisseurService;
+
+    @Autowired
+    private FournisseurRepository fournisseurRepository;
+
+    @BeforeEach
+    public void setUp() {
+        // Add any necessary setup here
+        // For example, you can create some sample data for testing
+
+        Fournisseur fournisseur1 = new Fournisseur();
+        fournisseur1.setCode("F1");
+        fournisseur1.setLibelle("Supplier 1");
+        fournisseur1.setCategorieFournisseur(CategorieFournisseur.ORDINAIRE);
+        fournisseurService.addFournisseur(fournisseur1);
+
+        Fournisseur fournisseur2 = new Fournisseur();
+        fournisseur2.setCode("F2");
+        fournisseur2.setLibelle("Supplier 2");
+        fournisseur2.setCategorieFournisseur(CategorieFournisseur.CONVENTIONNE);
+        fournisseurService.addFournisseur(fournisseur2);
+    }
+
+    @Test
+    public void testRetrieveAllFournisseurs() {
+        List<Fournisseur> fournisseurs = fournisseurService.retrieveAllFournisseurs();
+
+        // Add assertions to verify the results
+        assertNotNull(fournisseurs);
+        assertEquals(2, fournisseurs.size());
+    }
+
+    @Test
+    public void testAddFournisseur() {
+        Fournisseur fournisseur = new Fournisseur();
+        fournisseur.setCode("F3");
+        fournisseur.setLibelle("Supplier 3");
+        fournisseur.setCategorieFournisseur(CategorieFournisseur.ORDINAIRE);
+
+        // Call the method you want to test
+        Fournisseur addedFournisseur = fournisseurService.addFournisseur(fournisseur);
+
+        // Verify that the Fournisseur was saved and returned correctly
+        assertNotNull(addedFournisseur.getIdFournisseur());
+    }
+
+    @Test
+    public void testUpdateFournisseur() {
+        Fournisseur fournisseur = fournisseurRepository.findByCode("F1"); // Assuming a method to find by code exists
+
+        // Modify the Fournisseur object
+        fournisseur.setLibelle("Updated Supplier 1");
+
+        // Call the method you want to test
+        Fournisseur updatedFournisseur = fournisseurService.updateFournisseur(fournisseur);
+
+        // Verify that the Fournisseur was updated correctly
+        assertEquals("Updated Supplier 1", updatedFournisseur.getLibelle());
+    }
+
+    @Test
+    public void testDeleteFournisseur() {
+        Fournisseur fournisseur = fournisseurRepository.findByCode("F2"); // Assuming a method to find by code exists
+
+        // Call the method you want to test
+        fournisseurService.deleteFournisseur(fournisseur.getIdFournisseur());
+
+        // Verify that the Fournisseur with the specified ID was deleted
+        Fournisseur deletedFournisseur = fournisseurService.retrieveFournisseur(fournisseur.getIdFournisseur());
+        assertNull(deletedFournisseur);
+    }
+
+    @Test
+    public void testRetrieveFournisseur() {
+        Fournisseur fournisseur = fournisseurRepository.findByCode("F1"); // Assuming a method to find by code exists
+
+        // Call the method you want to test
+        Fournisseur retrievedFournisseur = fournisseurService.retrieveFournisseur(fournisseur.getIdFournisseur());
+
+        // Verify that the correct Fournisseur was retrieved
+        assertNotNull(retrievedFournisseur);
+        assertEquals("F1", retrievedFournisseur.getCode());
+    }
+}
